4c556de75105bdbf23ed23143c3a9da47e399596
kesavan <kesavanplk7@gmail.com>
2024-06-21 02:58:54 -0400

    add env file and secret port number added

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
-import express from "express";
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+require("dotenv").config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
